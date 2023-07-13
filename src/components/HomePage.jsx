5c52b03588b0ee12bd4798af2649302fba598217
Bishal De <itsbishalde@gmail.com>
2023-07-13 21:37:24 +0530

    homebox fixed2

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from './Navbar/Navbar'
-import Homebox from './HomeBox/Homebox'
+import Homebox from './Homebox/Homebox'
 import Contentbox from './Contentbox/Contentbox'
 
 function HomePage() {
